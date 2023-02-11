a4ddadcf5e2323c66a5271d10838dfdcd14d7c2f
Кислый Кислюк <kislball@yahoo.com>
2023-02-12 00:00:50 +0300

    some head.c stuff

--- enalang_cgen/src/head.c
+++ enalang_cgen/src/head.c
@@ -22,7 +22,7 @@ union ena_value_inner {
     double number;
     char * string;
     bool boolean;
-    size_t pointer;
+    struct ena_value * pointer;
     struct ena_value * exception;
     char * atom;
     void * null;
@@ -51,7 +51,58 @@ void realloc_stack() {
     stack_begin = new_stack;
 }
 
+void push_stack(struct ena_value val) {
+    if (stack_size >= stack_capacity) {
+        realloc_stack();
+    }
+    stack_size++;
+    stack_begin[stack_size - 1] = val;
+}
+
+void alloc() {
+    struct ena_value * new_mem = malloc(sizeof(struct ena_value));
+    if (new_mem == NULL) {
+        fprintf(stderr, "failed to allocate memory for ena heap");
+        exit(1);
+    }
+    
+    union ena_value_inner inner = {
+        .pointer = new_mem,
+    };
+    struct ena_value val = {
+        .type = ena_pointer,
+        .value = inner
+    };
+    push_stack(val);
+}
+
+void free_value(struct ena_value val) {
+    switch (val.type) {
+        case ena_string:
+        case ena_pointer:
+        case ena_atom:
+        case ena_exception:
+            free(val.value.atom);
+        default:
+            break;
+    }
+}
+
+struct ena_value pop_stack() {
+    if (stack_size == 0) {
+        fprintf(stderr, "stack ended");
+        exit(1);
+    }
+    stack_size -= 1;
+    struct ena_value val = stack_begin[stack_size];
+    return val;
+}
+
 void free_stack() {
+    for (size_t i = 0; i < stack_size; ++i) {
+        struct ena_value val = stack_begin[i];
+        free_value(val);
+    }
     free(stack_begin);
 }
 
