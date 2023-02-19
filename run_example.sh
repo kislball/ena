d9520352059a8d6bea1f6e4fbbe3a27e6c16ec4f
Кислый Кислюк <kislball@yahoo.com>
2023-02-19 16:37:34 +0300

    some stuff

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-ena compile ./std/base/**/*.ena ./std/vm/**/*.ena ./examples/$1.ena
+ena compile "./std/base/**/*.ena ./std/vm/**/*.ena" -o ./std.enair
+ena compile ./examples/$1.ena -o ./main.enair
+ena link ./main.enair ./std.enair -o output.enair
 ena run ./output.enair
-rm output.enair
+rm output.enair main.enair std.enair
